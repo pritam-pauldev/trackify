e50239bd1cbd3a75118d8c9f7da0be8c3fc84b86
Pritam Paul <pritampaulsdev@gmail.com>
2026-04-22 18:06:36 +0530

    migration implemented to test by adding note column in users

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,31 +2,31 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   try {
-    // ✅ LINE 1: Get token from header
+    // Get token from header
     const authHeader = req.headers.authorization;
 
-    // ✅ LINE 2: Check if token exists
+    // Check if token exists
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res
         .status(401)
         .json({ message: "Access denied. No token provided." });
     }
 
-    // ✅ LINE 3: Extract token
+    // Extract token
     const token = authHeader.split(" ")[1];
 
-    // ✅ LINE 4: Verify token
+    // Verify token
     const decoded = jwt.verify(
       token,
       "1q2waSE34rdZXcG457HVnjuY67InKu89PlIFYU64SRTUinvcd4679OJfr",
     );
 
-    // ✅ LINE 5: Attach user info to request
+    // Attach user info to request
     req.user = decoded; // { userId: ... }
 
     next(); // move to controller
   } catch (error) {
-      console.log("JWT ERROR:", error.message);
+    console.log("JWT ERROR:", error.message);
     return res.status(401).json({ message: "Invalid or expired token." });
   }
 };
